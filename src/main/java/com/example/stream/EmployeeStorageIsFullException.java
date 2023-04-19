b6f77941879902c8121962f42a049fe6323897b3
Аня <grigorevna.antoninka@mail.ru>
2023-04-19 13:07:56 +0700

    домашняя 2.8

---
+++ src/main/java/com/example/stream/EmployeeStorageIsFullException.java
@@ -0,0 +1,7 @@
+package com.example.stream;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+public class EmployeeStorageIsFullException extends RuntimeException{
+}
